256c0787932559430a875c61493d6a0622bf1a56
Hasanat Jahan <jahanhasanat@gmail.com>
2021-04-21 23:48:29 -0400

    NB.py is taking in the files and doing label and o document count

--- NB.py
+++ NB.py
@@ -1,5 +1,3 @@
-import json
-
 """
 should take the following parameters: 
 - the training file 
@@ -15,12 +13,44 @@ first column is the label,
 and the other columns are feature values.
 """
 
+import json
+
 # Here we would have input from the command line but for how we have placeholders 
 
 
 # trying with the small file 
-training_file = '/Users/jahan/Desktop/CS381/Homework2/small_movie_review/feature_vectors'
+training_file = '/Users/jahan/Desktop/CS381/Homework2/small_movie_review/feature_vectors/train_feature_vectors.json'
+testing_file = '/Users/jahan/Desktop/CS381/Homework2/small_movie_review/feature_vectors/test_feature_vectors.json'
+parameter_file = '/Users/jahan/Desktop/CS381/Homework2/small_movie_review/movie_review_small.NB'
+output_file = '/Users/jahan/Desktop/CS381/Homework2/small_movie_review/output.txt'
+
+
+def build_parameter_file(training_file, parameter_file):
+    # open training file and read through it 
+    f = open(training_file)
+    training_data = json.load(f)
+    f.close()
+
+    # get the number of documents 
+    # print(training_data)
+    num_document = len(training_data)
+
+    label_dict = dict()
+
+    # iterate through the dictionary to draw out the labels to create prior probability 
+    for i in training_data:
+        for j in i:
+            if j not in label_dict:
+                label_dict[j] = 1
+            else:
+                label_dict[j] += 1  
+    
 
 
 def run_naive_bayes(training_file, testing_file, parameter_file, output_prediction_file):
     print("running")
+
+
+build_parameter_file(training_file, parameter_file)
+run_naive_bayes(training_file, testing_file, parameter_file, output_file)
+
